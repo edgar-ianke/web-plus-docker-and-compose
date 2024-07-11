7d5c26184fddf6e60d56181bdc97091ef29c3452
edgar-ianke <edgaryanke@yandex.ru>
2024-07-11 10:09:30 +0300

    new backend

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -6,11 +6,11 @@ import { Wishlist } from '../wishlists/entities/wishlist.entity';
 export default () => ({
   database: {
     type: 'postgres' as const,
-    host: process.env.POSTGRES_HOST || 'localhost',
-    port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-    username: process.env.POSTGRES_USER || 'edgar',
-    password: process.env.POSTGRES_PASSWORD || 'edgar',
-    database: process.env.POSTGRES_DB || 'kupipodaridai',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    username: process.env.DB_USERNAME || 'edgar',
+    password: process.env.DB_PASSWORD || 'edgar',
+    database: process.env.DB_NAME || 'kupipodaridai',
     entities: [User, Wish, Offer, Wishlist],
     synchronize: true,
   },
